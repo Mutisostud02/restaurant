7e72252242ae77318d68e1b4d2db30f05c6a5893
Mutisostud02 <mutiso8395@gmail.com>
2023-09-19 13:23:48 +0300

    Added function to populate the page with new content when menu tab is clicked imported menu.js modules to populate the page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import home from './home';
+import {menuTabPage as menuContent} from './menu';
+import {header as head} from './menu';
 import './styles.css';
 
-
 const content = document.querySelector('.content');
-content.appendChild(home());
-console.log('hello, this is my project');
+content.setAttribute('style','overflow:hidden;height:100vh;margin:0;')
+window.onload = content.appendChild(home());
+const contentSide = document.querySelector('.contentSide');
+// contentSide.setAttribute('style','width:80vw; display:flex; margin:0; flex-direction:column; height:auto; gap:0.3vh;margin:0;');
+
+const contentDivs = document.querySelector('.contentDivs');
+const header = document.querySelector('.header');
+
+const menuTab = document.querySelector('.menu');
+menuTab.setAttribute('style','margin-bottom:5vh;')
+menuTab.addEventListener('click',()=> {
+    header.remove();
+    contentDivs.remove();
+    contentSide.appendChild(head());
+    contentSide.appendChild(menuContent())
+
+}, {once: true});
+
+
+
+// .addEventListener('click',() => {
+//     content.removeChild(home());
+//     content.appendChild(menu());
+// })
+
